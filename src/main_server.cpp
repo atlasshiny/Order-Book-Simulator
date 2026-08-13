fa4331dcde2096fadf0ccf71710af89e4eb9cac8
shinyatlas <mannyoso07@gmail.com>
2026-08-12 20:38:09 -0400

    added symbols/new orderbooks to the server runtime

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -1,6 +1,8 @@
 #include "network/TCPServer.hpp"
 #include "orchestrator/ExchangeOrchestrator.hpp"
 #include "gateways/FIXGateway.hpp"
+#include "orderbook/Symbol.hpp"
+#include <string>
 #include <memory>
 #include <iostream>
 
@@ -12,6 +14,13 @@ int main() {
         // Create the business logic component
         auto gateway = std::make_unique<FIXGateway>();
         auto orchestrator = std::make_shared<ExchangeOrchestrator>(std::move(gateway));
+
+        // Add order books for specific symbols
+        orderbook::Symbol AAPL_symbol = orderbook::make_symbol("AAPL");
+        orderbook::Symbol NVDA_symbol = orderbook::make_symbol("NVDA");
+
+        orchestrator->addOrderBook(orderbook::Symbol(AAPL_symbol));
+        orchestrator->addOrderBook(orderbook::Symbol(NVDA_symbol));
 
         // Start the server (this hooks up the internal io_context acceptor loop)
         TCPServer server(port, orchestrator);
