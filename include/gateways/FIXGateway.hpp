6aa701d8d9f9b568176821686849a45d43e07ae4
shinyatlas <mannyoso07@gmail.com>
2026-08-13 20:27:49 -0400

    added set method for the orchestrator that is being used in FIXGateway. additionally, ensured that all FIX tag definitions were being properly taken care of

--- include/gateways/FIXGateway.hpp
+++ include/gateways/FIXGateway.hpp
@@ -4,6 +4,8 @@
 #include "parsers/FIXParser.hpp"
 #include "writers/FIXWriter.hpp"
 
+class ExchangeOrchestrator; // Forward declaration
+
 class FIXGateway : public IGateway {
 public:
     std::optional<Order> on_data_received(std::shared_ptr<TCPSession> session, std::string_view raw_data) override;
@@ -14,13 +16,14 @@ public:
     FIXWriter& get_writer();
     FIXParser& get_parser() { return fixParser_; }
 
+    void set_orchestrator(std::shared_ptr<ExchangeOrchestrator> orchestrator);
+
 private:
     FIXWriter fixWriter_;
     FIXParser fixParser_;
+    std::shared_ptr<ExchangeOrchestrator> orchestrator_;
 
     size_t sendOrder(const Order& order, char* wireBuffer_, size_t bufferSize) override;
 
     std::optional<Order> receiveOrder(std::string_view rawData) override;
-
-    void cancelOrder(int orderId) override;
 };
